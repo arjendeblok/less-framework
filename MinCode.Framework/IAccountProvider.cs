7ca7dfdb28a890928a5140aba62e47eb2fad5d01
Arjen de Blok <arjen@deblok.org>
2023-12-08 18:19:16 +0100

    Added account and audit provider

---
+++ MinCode.Framework/IAccountProvider.cs
@@ -0,0 +1,24 @@
+﻿namespace MinCode.Framework
+{
+  public interface IAccountProvider
+  {
+    /// <summary>
+    /// Indicates if there is an authenticated user
+    /// </summary>
+    bool IsAuthenticated { get; }
+    /// <summary>
+    /// Returns the username
+    /// </summary>
+    string UserName { get; }
+    /// <summary>
+    /// Returns the full name of the user
+    /// </summary>
+    string FullName { get; }
+    /// <summary>
+    /// Determines if the user has a specific right
+    /// </summary>
+    /// <param name="right">right to check</param>
+    /// <returns></returns>
+    bool HasRight(string right);
+  }
+}
---
+++ MinCode.Framework/IAuditProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace MinCode.Framework
+{
+  /// <summary>
+  /// Result of an operation
+  /// </summary>
+  public enum AuditResult
+  {
+    Handled, Unauthenticated, Unauthorized, Failed
+  }
+
+  /// <summary>
+  /// Interface to an audit provider
+  /// </summary>
+  public interface IAuditProvider
+  {
+    /// <summary>
+    /// Indicates if auditing is enabled
+    /// </summary>
+    bool IsAuditEnabled { get; }
+
+    /// <summary>
+    /// Audits a command given by a user
+    /// </summary>
+    /// <typeparam name="TCommand"></typeparam>
+    /// <param name="result">Result of the handling</param>
+    /// <param name="message">Message to log</param>
+    /// <param name="user">User initiated the command</param>
+    /// <param name="command">The command executed</param>
+    /// <returns></returns>
+    Task AuditAsync<TCommand>(AuditResult result,
+      string message, string user, TCommand? command)
+      where TCommand : class, ICommand;
+  }
+}
