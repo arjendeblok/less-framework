eb116dc3e35346946eddfbb6999534f4ebc330ed
Arjen de Blok <arjen@deblok.org>
2023-12-10 21:29:36 +0100

    Added command validator

---
+++ MinCode.Framework/ICommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace MinCode.Framework
+{
+  /// <summary>
+  /// Validates a command
+  /// </summary>
+  /// <typeparam name="TCommand">Type of the command</typeparam>
+  public interface ICommandValidator<in TCommand>
+    : IValidator<TCommand>
+    where TCommand : ICommand
+  {
+  }
+}
