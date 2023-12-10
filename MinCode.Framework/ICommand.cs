5510c9bf67d14fccdbfa8cc30291d06d163c7446
Arjen de Blok <arjen@deblok.org>
2023-12-10 05:00:00 +0100

    Added comment

--- MinCode.Framework/ICommand.cs
+++ MinCode.Framework/ICommand.cs
@@ -3,6 +3,9 @@
   /// <summary>
   /// Tag to see this (record) class as a command
   /// </summary>
+  /// <remarks>
+  /// This framework assumes the one type of a command belongs to zero or one type of entity
+  /// </remarks>
   public interface ICommand
   {
   }
